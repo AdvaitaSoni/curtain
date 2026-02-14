89ff31d93b48ce96eb072bc4ba7caed87d876b04
Advaita Soni <advaitasoni1024@gmail.com>
2026-02-15 01:57:34 +0530

    ADDED OPTION TO ADD SHORTCUTS FOR OTHER APPS

--- extension.js
+++ extension.js
@@ -1,42 +1,50 @@
 const key = require("./keyhandler");
+const Util = imports.misc.util;
 
 //------------------------ GLOBAL OBJECTS
 let extension;
 
 //------------------------- EXTENSION
 class myExtension {
-  keyHandler;
-  constructor() {
-    this.keyHandler = new key.KeyHandler();
-  }
-  destroy() {
-    this.keyHandler.destroy();
-    this.keyHandler = null;
-  }
+    keyHandler;
+    constructor() {
+        this.keyHandler = new key.KeyHandler();
+    }
+    destroy() {
+        this.keyHandler.destroy();
+        this.keyHandler = null;
+    }
 }
 
 // -------------------------------SETUP FUNCTIONS
 function init(metadata) {
-  try {
-    extension = new myExtension();
-  } catch (e) {
-    global.log("error in init function ", e.message);
-  }
+    try {
+        extension = new myExtension();
+    } catch (e) {
+        global.log("error in init function ", e.message);
+    }
 }
 
 function enable() {
-  try {
-    if (!extension) extension = new myExtension();
-  } catch (e) {
-    global.log("error in enable function ", e.message);
-  }
+    try {
+        if (!extension) extension = new myExtension();
+    } catch (e) {
+        global.log("error in enable function ", e.message);
+    }
+    return Callbacks
 }
 
 function disable() {
-  try {
-    extension.destroy();
-    extension = null;
-  } catch (e) {
-    global.log("error in disable function ", e.message);
-  }
+    try {
+        extension.destroy();
+        extension = null;
+    } catch (e) {
+        global.log("error in disable function ", e.message);
+    }
+}
+
+const Callbacks = {
+    custom_shortcuts: function() {
+        Util.spawnCommandLineAsync("cinnamon-settings keyboard -t shortcuts");
+    }
 }
