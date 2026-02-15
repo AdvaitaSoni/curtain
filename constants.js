6e5b9b0b61113abb2d6b68b7b7894b7932457b1b
Advaita Soni <advaitasoni1024@gmail.com>
2026-02-15 12:42:25 +0530

    modified name of the extension

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 const EVENT_GAP = 50;
-const UUID = "tileRice@AdvaitaSoni";
+const UUID = "curtain@AdvaitaSoni";
 const ANIMATION_STATUS = true
 const ANIMATION_TYPE = "linear"
 const ANIMATION_TIME = 0.1
